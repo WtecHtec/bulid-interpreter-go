715f4faf4d857155412f6b92136b2b2e48a94010
herzshen <herzshen@tencent.com>
2022-08-02 19:39:18 +0800

    表达式

--- parser/parser.go
+++ parser/parser.go
@@ -7,6 +7,23 @@ import (
 	"monkey/ast"
 	"monkey/lexer"
 	"monkey/token"
+	"strconv"
+)
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -X or !X
+	CALL        // myFunction(X)
+)
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
 )
 
 type Parser struct {
@@ -14,6 +31,9 @@ type Parser struct {
 	errors    []string
 	curToken  token.Token // 当前
 	peekToken token.Token // 窥视下一个
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -22,7 +42,11 @@ func New(l *lexer.Lexer) *Parser {
 	// 初始化 读取两次token，curToken跟 peekToken 都设置好
 	p.nextToken()
 	p.nextToken()
-
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.BANG, p.parsePrefixExpression)
+	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	return p
 }
 
@@ -72,8 +96,6 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	}
 }
 
-// parser/parser.go
-
 func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
@@ -81,8 +103,29 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStatement()
 	}
+}
+
+// func (p *Parser) parseExpression(precedence int) ast.Expression {
+// 	prefix := p.prefixParseFns[p.curToken.Type]
+// 	if prefix == nil {
+// 		return nil
+// 	}
+// 	leftExp := prefix()
+
+// 	return leftExp
+// }
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
@@ -119,4 +162,62 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	lit.Value = value
+
+	return lit
+}
+
+// parser/parser.go
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType) {
+	msg := fmt.Sprintf("no prefix parse function for %s found", t)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil {
+		p.noPrefixParseFnError(p.curToken.Type)
+		return nil
+	}
+	leftExp := prefix()
+
+	return leftExp
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression {
+	expression := &ast.PrefixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+	}
+
+	p.nextToken()
+
+	expression.Right = p.parseExpression(PREFIX)
+
+	return expression
 }
--- test_exp/test.go
+++ test_exp/test.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Lexer struct {
 	input        string
@@ -12,6 +14,32 @@ type Lexer struct {
 func test() {
 	fmt.Print("hello test")
 }
+
+//定义函数类型
+type handler func(name string) int
+
+//实现函数类型方法
+func (h handler) add(name string) int {
+	return h(name) + 10
+}
+
+type Retriever interface {
+	Get(url string) string
+	Add(num int) int
+}
+
+type Retrievers struct {
+	name string
+}
+
+func (r *Retrievers) Get(url string) string {
+	return string(url)
+}
+
+func (r *Retrievers) Add(num int) int {
+	return num
+}
+
 func main() {
 	test()
 	l := &Lexer{input: "7777"}
@@ -21,4 +49,20 @@ func main() {
 	n.ch = '6'
 	fmt.Print(l)
 	fmt.Print(n)
+
+	//注意要成为函数对象必须显式定义handler类型
+	var my handler = func(name string) int {
+		return len(name)
+	}
+	fmt.Print(my("777"))
+	fmt.Println(my.add("taozs")) //调用函数对象的方法
+
+	var r Retriever
+	r = &Retrievers{name: "测试"}
+	// fmt.Println(download(r))
+
+	// Type Assertion
+	if retriever, ok := r.(*Retrievers); ok {
+		fmt.Printf("%T %v\n", retriever, retriever.name)
+	}
 }
