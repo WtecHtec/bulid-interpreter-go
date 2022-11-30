6df3680c106a56e70046302e71b88e56a6a71bcf
herzshen <herzshen@tencent.com>
2022-11-30 17:43:23 +0800

    嵌套函数

--- lg-chinese/index.js
+++ lg-chinese/index.js
@@ -5,13 +5,14 @@ const Paser = require('./paser')
 const code = ` 
  设 x 为  5; 
  y 等于 8 乘 x; 
+ z 等于 8;
  设 add 为 方程(x, y) {
   y 等于 8;
   设 db 为 方程(y) {
     返回 y 乘 2;
   };
-  打印 y;
-  返回 x 乘 y;
+  打印 x,y,z;
+  返回 x 乘 y 乘 z;
  };
  res 等于 add(2, y);
  打印 res,  x 乘 add(2, y);`
